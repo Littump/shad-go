b329817db25b6787d941b1e1870f448a0a1aa3bf
Yakimov Roman <85906769+Littump@users.noreply.github.com>
2024-11-17 23:43:39 +0300

    -

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,8 @@
 
 package hotelbusiness
 
+import "sort"
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +15,30 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	exchangeGuests := make(map[int]int)
+
+	for _, guest := range guests {
+		exchangeGuests[guest.CheckInDate] += 1
+		exchangeGuests[guest.CheckOutDate] -= 1
+	}
+
+	dates := make([]int, 0, len(exchangeGuests))
+	for date := range exchangeGuests {
+		dates = append(dates, date)
+	}
+	sort.Ints(dates)
+
+	loads := make([]Load, 0)
+	currentGuestCount := 0
+
+	for _, date := range dates {
+		guestCountChange := exchangeGuests[date]
+		currentGuestCount += guestCountChange
+		if len(loads) > 0 && loads[len(loads)-1].GuestCount == currentGuestCount {
+			continue
+		}
+		loads = append(loads, Load{date, currentGuestCount})
+	}
+
+	return loads
 }
--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,27 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	args := os.Args
+	m := make(map[string]int)
+	for _, arg := range args {
+		dat, _ := os.ReadFile(arg)
+		s := string(dat)
+
+		for _, word := range strings.Fields(s) {
+			m[word]++
+		}
+	}
+
+	for k, v := range m {
+		if v >= 2 {
+			fmt.Printf("%v\t%s\n", v, k)
+		}
+	}
 }
